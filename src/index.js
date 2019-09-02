7e8885067c7cb534b2dc55d755c5e1258269d8b8
Arthur Chen <achen1738@gmail.com>
2019-09-02 11:24:27 -0500

    Populate

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ import { Provider } from "react-redux";
 import configureStore from "./modules/app/store";
 const store = configureStore();
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById("root")
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
